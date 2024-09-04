828d3e3e85fd11eeff3bd48072df49259b7197cf
M Alamin <alamin.nava@gmail.com>
2024-09-04 16:59:37 +0600

    Note Taking app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,85 @@
 import {useState} from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [counter2, setCounter2] = useState(0);
-    const increaseHandler = () =>{
-        setCounter(counter +1)
+    const [noteTitle, setNoteTitle] = useState('')
+    const [notes, setNotes] = useState([
+        {id:1, title: "note 1"},
+        {id:3, title: "note 2"},
+    ])
+    const [editMode, setEditMode] = useState(false)
+    const [editableNote, setEditableNote] = useState(null)
+    // hangle on change
+    let handleOneChange = (e) => {
+        setNoteTitle(e.target.value)
     }
-    const decreaseHandler = () =>{
-        setCounter(counter -1)
+    // handleSubmit
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if(noteTitle.trim() === ''){
+            return alert('Please Provide some Title')
+        }
+        editMode ? handleUpdated() : handleCreate();
     }
-    const increaseHandler2 = () =>{
-        setCounter2(counter2 +1)
-    }
-    const decreaseHandler2 = () =>{
-        setCounter2(counter2 -1)
+    // handleCreate
+    const handleCreate = () =>{
+        const newNote = {
+            id: Date.now() + "",
+            title: noteTitle,
+        };
+        setNotes([newNote, ...notes])
+        setNoteTitle('')
     }
 
-
+    // handleRemove
+    const handleRemove = (noteId) =>{
+        const updateNote = notes.filter((item)=> item.id !== noteId)
+        setNotes(updateNote)
+    }
+    // handle Edit
+    const handleEdit = (note) =>{
+        setEditMode(true)
+        setNoteTitle(note.title)
+        setEditableNote(note)
+    }
+        // handleUpdated
+        const handleUpdated = () =>{
+            const updatedNotes = notes.map((item) =>{
+                if(item.id == editableNote.id){
+                    return {...item, title: noteTitle}
+                }
+                return item;
+            })
+            setEditMode(false)
+            setNotes(updatedNotes)
+            setNoteTitle('')
+        }
     return (
-        <>
-            <div>
-                The value of the counter {counter}
-                <div>
-                <button onClick={increaseHandler}>Increase by 1</button>
-                <button onClick={decreaseHandler}>Decrease by 1</button>
-                </div>
-            </div>
-            <br/>
-            <div>
-                The value of the counter {counter2} Other state
-                <div>
-                <button onClick={increaseHandler2}>Increase by 1</button>
-                <button onClick={decreaseHandler2}>Decrease by 1</button>
-                </div>
-            </div>
-        </>
+        <div className="container p-5">
+            <form onSubmit={handleSubmit} className="d-flex mb-3">
+                <input onChange={handleOneChange} type="text" className="form-control" value={noteTitle} style={{maxWidth:"300px"}}/>
+                <button type="submit" className="btn btn-primary">{editMode ? "Update Note" : "Add Note"}</button>
+            </form>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Note Title</th>
+                        <th scope="col">Edit Note</th>
+                        <th scope="col">Remove Note</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        {
+                            notes.map((note)=>(
+                                <tr key={note.id}>
+                                    <td>{note.title}</td>
+                                    <td><button onClick={()=>handleEdit(note)} className="btn btn-secondary">Edit Note</button> </td>
+                                    <td><button onClick={()=>handleRemove(note.id)} className="btn btn-danger">Remove Note</button></td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+        </div>
     )
 }
 
