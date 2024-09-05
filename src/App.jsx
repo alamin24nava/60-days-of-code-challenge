e1623dfc4a29babd6f4452190124829ec2cf53cf
M Alamin <alamin.nava@gmail.com>
2024-09-05 16:28:39 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,96 @@
 import {useState} from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-    const [noteTitle, setNoteTitle] = useState('')
-    const [notes, setNotes] = useState([])
-    const [editMode, setEditMode] = useState(false)
-    const [editableNote, setEditableNote] = useState(null)
-    // hangle on change
-    let handleOneChange = (e) => {
-        setNoteTitle(e.target.value)
-    }
-    // handleSubmit
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        if(noteTitle.trim() === ''){
-            return alert('Please Provide some Title')
-        }
-        editMode ? handleUpdated() : handleCreate();
-    }
-    // handleCreate
-    const handleCreate = () =>{
-        const newNote = {
-            id: Date.now() + "",
-            title: noteTitle,
-        };
-        setNotes([newNote, ...notes])
-        setNoteTitle('')
-    }
+    const randomNumberInRange = () => {
+        return Math.floor(Math.random() * 10);
+    };
+    const [user, setUser] = useState('');
+    const [luckyNumber, setLuckyNumber] = useState('');
+    const [randomNum, setRandomNum] = useState(0);
+    const [attempt, setAttempt] = useState(1);
+    const [userList, setUserList] = useState([])
+    const [result, setResult] = useState('')
+    const [test, setTest] = useState('')
+    // Generate a Random Number
 
-    // handleRemove
-    const handleRemove = (noteId) =>{
-        const updateNote = notes.filter((item)=> item.id !== noteId)
-        setNotes(updateNote)
+    // handle Change Name
+    const handleChangeName = (e) =>{
+        setUser(e.target.value)
     }
-    // handle Edit
-    const handleEdit = (note) =>{
-        setEditMode(true)
-        setNoteTitle(note.title)
-        setEditableNote(note)
+    // handle Change Number
+    const handleChangeNumber = (e) =>{
+        setLuckyNumber(e.target.value)
     }
-        // handleUpdated
-        const handleUpdated = () =>{
-            const updatedNotes = notes.map((item) =>{
-                if(item.id == editableNote.id){
-                    return {...item, title: noteTitle}
-                }
-                return item;
-            })
-            setEditMode(false)
-            setNotes(updatedNotes)
-            setNoteTitle('')
+    // handle Submit
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        if(user === ''){
+            return alert('Please Provide Name')
         }
+        if(luckyNumber === ''){
+            return alert('Please Provide Your Lucky Number')
+        }
+        setRandomNum(randomNumberInRange());
+        const newList = {
+            id: Date.now() + "",
+            user: user,
+            result:result,
+        }
+        setUserList([newList, ...userList])
+        setUser('')
+        setLuckyNumber('')
+        setAttempt(attempt + 1)
+
+        if(randomNum == luckyNumber && attempt == 1){
+            setResult(`${attempt}st Time Win`)
+        }else if(randomNum == luckyNumber && attempt == 2){
+            setResult(`${attempt}nd Time Win`)
+        }else if(randomNum == luckyNumber && attempt == 3){
+            setResult(`${attempt}rd Time Win`)
+        }else{
+            setResult(`N/A`)
+        }
+
+   
+
+        console.log(`random ${randomNum}`)
+        console.log(`luck ${luckyNumber}`)
+    }
     return (
-        <div className="container p-5">
-            <form onSubmit={handleSubmit} className="d-flex mb-3">
-                <input onChange={handleOneChange} type="text" className="form-control" value={noteTitle} style={{maxWidth:"300px"}}/>
-                <button type="submit" className="btn btn-primary">{editMode ? "Update Note" : "Add Note"}</button>
+        
+        <div className="w-25 mx-auto m-5">
+            <form onSubmit={handleSubmit}>
+                {/* <input onChange={handleChangeName} value={user} type="text" className="form-control mb-2" placeholder="Enter Name"/> */}
+                <input onChange={handleChangeName} value={user} type="text" className={`${attempt <= 3 ? 'form-control mb-2':'form-control mb-2 disabled'}`} placeholder="Enter Name"/>
+                <input onChange={handleChangeNumber} value={luckyNumber} type="number" className="form-control text-center mb-2" maxLength={2} placeholder="Enter Your Lucky Number" style={{height:'150px'}}/>
+                { attempt <= 3 ? (
+                    <button className="btn btn-primary" type="submit">Try Your Luck</button>
+                ) : null}
             </form>
+           
+            { attempt <= 3 ? (
+                <p><span></span> You Can Try {attempt} / 3  Times</p>
+            ) : <p className="text-danger">You are Loss</p>}
+
+            {userList.length !== 0 ? (
             <table className="table">
                 <thead>
                     <tr>
-                        <th scope="col">Note Title</th>
-                        <th scope="col">Edit Note</th>
-                        <th scope="col">Remove Note</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Attempt</th>
                     </tr>
                 </thead>
-                    <tbody>
-                        {
-                            notes.map((note)=>(
-                                <tr key={note.id}>
-                                    <td>{note.title}</td>
-                                    <td><button onClick={()=>handleEdit(note)} className="btn btn-secondary">Edit Note</button> </td>
-                                    <td><button onClick={()=>handleRemove(note.id)} className="btn btn-danger">Remove Note</button></td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+                <tbody>
+                    {
+                        userList.map((singleUser) => 
+                            <tr key={singleUser.id}>
+                                <td>{singleUser.user}</td>
+                                <td>{singleUser.result}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>):null}
         </div>
     )
 }
