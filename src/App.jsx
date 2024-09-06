44a65a38e5c70349639e3c6a43035a0cd5d37d24
M Alamin <alamin.nava@gmail.com>
2024-09-06 18:27:44 +0600

    rebuild attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,73 @@
-import {useState} from "react"
+import {useRef, useState} from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-    const randomNumberInRange = () => {
-        return Math.floor(Math.random() * 10);
-    };
-    const [user, setUser] = useState('');
-    const [luckyNumber, setLuckyNumber] = useState('');
-    const [randomNum, setRandomNum] = useState(0);
-    const [attempt, setAttempt] = useState(1);
-    const [userList, setUserList] = useState([])
-    const [result, setResult] = useState('')
-    const [test, setTest] = useState('')
-    // Generate a Random Number
-
-    // handle Change Name
-    const handleChangeName = (e) =>{
-        setUser(e.target.value)
+    const [userName, setUserName] = useState('')
+    const [luckyNumber, setLuckyNumber] = useState("")
+    const [count, setCount] = useState(3)
+    const [msg, setMsg] = useState('')
+    const [users, setUsers] = useState([
+        {id:1, name:'alamin', result:'Win'}
+    ]);
+    // handleUserName
+    const handleUserName = (e) =>{
+        setUserName(e.target.value)
     }
-    // handle Change Number
-    const handleChangeNumber = (e) =>{
+    // handleLuckyNumber
+    const handleLuckyNumber = (e) =>{
         setLuckyNumber(e.target.value)
     }
-    // handle Submit
-    const handleSubmit =(e)=>{
+    const restAll = () => {
+        setUserName("");
+        setLuckyNumber("");
+        setCount(3);
+        setTimeout(()=>{
+            setMsg("");
+        },2000)
+    }
+
+    // handleSubmit
+    const handleSubmit = (e) =>{
         e.preventDefault();
-        if(user === ''){
-            return alert('Please Provide Name')
+        if(userName == '' || luckyNumber == ''){
+            return alert('Please fill required field')
         }
-        if(luckyNumber === ''){
-            return alert('Please Provide Your Lucky Number')
-        }
-        setRandomNum(randomNumberInRange());
-        const newList = {
-            id: Date.now() + "",
-            user: user,
-            result:result,
-        }
-        setUserList([newList, ...userList])
-        setUser('')
-        setLuckyNumber('')
-        setAttempt(attempt + 1)
+        if(count > 0){
+            let currentCount = 4-count;
+            setCount(count - 1);
+            let rand = Math.floor(Math.random() * 1);
+            if(rand == luckyNumber){
+                const user = {
+                    id: Date.now(),
+                    name: userName,
+                    result: currentCount,
+                }
+                setUsers([user, ...users])
+                setMsg("Success You win")
+                restAll()
+            }
 
-        if(randomNum == luckyNumber && attempt == 1){
-            setResult(`${attempt}st Time Win`)
-        }else if(randomNum == luckyNumber && attempt == 2){
-            setResult(`${attempt}nd Time Win`)
-        }else if(randomNum == luckyNumber && attempt == 3){
-            setResult(`${attempt}rd Time Win`)
-        }else{
-            setResult(`N/A`)
+            if(count <= 1){
+                const user = {
+                    id: Date.now(),
+                    name: userName,
+                    result: 'N/A',
+                }
+                setUsers([user, ...users])
+                setMsg("Try again")
+                restAll()
+            }
         }
-
-   
-
-        console.log(`random ${randomNum}`)
-        console.log(`luck ${luckyNumber}`)
     }
     return (
         
         <div className="w-25 mx-auto m-5">
             <form onSubmit={handleSubmit}>
-                {/* <input onChange={handleChangeName} value={user} type="text" className="form-control mb-2" placeholder="Enter Name"/> */}
-                <input onChange={handleChangeName} value={user} type="text" className={`${attempt <= 3 ? 'form-control mb-2':'form-control mb-2 disabled'}`} placeholder="Enter Name"/>
-                <input onChange={handleChangeNumber} value={luckyNumber} type="number" className="form-control text-center mb-2" maxLength={2} placeholder="Enter Your Lucky Number" style={{height:'150px'}}/>
-                { attempt <= 3 ? (
-                    <button className="btn btn-primary" type="submit">Try Your Luck</button>
-                ) : null}
+                <input onChange={handleUserName} value={userName} type="text" className="form-control mb-2" placeholder="Enter Name"/>
+                <input onChange={handleLuckyNumber} value={luckyNumber} type="number" className="form-control text-center mb-2" placeholder="Enter Your Lucky Number" style={{height:'150px'}}/>
+                <button className="btn btn-primary w-100" type="submit">Try Your Luck</button>
             </form>
-           
-            { attempt <= 3 ? (
-                <p><span></span> You Can Try {attempt} / 3  Times</p>
-            ) : <p className="text-danger">You are Loss</p>}
-
-            {userList.length !== 0 ? (
+           <p>{msg && msg}</p>
+           <p>{count} attempt remaining</p>                   
             <table className="table">
                 <thead>
                     <tr>
@@ -82,15 +77,16 @@ function App() {
                 </thead>
                 <tbody>
                     {
-                        userList.map((singleUser) => 
-                            <tr key={singleUser.id}>
-                                <td>{singleUser.user}</td>
-                                <td>{singleUser.result}</td>
+                        users.map((data) =>
+                            <tr key={data.id}>
+                                <td>{data.name}</td>
+                                <td>{data.result}</td>
                             </tr>
                         )
                     }
+                    
                 </tbody>
-            </table>):null}
+            </table>
         </div>
     )
 }
