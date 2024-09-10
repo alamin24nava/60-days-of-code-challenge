c7c5d30a9a19d92f447c82dd7d09d1b6d0c5e0fd
M A AMIN (Home) <alamin.nava@gmail.com>
2024-09-10 22:53:50 +0600

    Basic Concept clear useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import StudentForm from "./components/StudentForm";
-import StudentsList from "./components/StudentsList";
-import StudentsStatus from "./components/StudentsStatus";
+import { useReducer } from 'react';
+const counterReducer =(state, action)=>{
+    switch(action.type){
+        case "increase":{
+            return state + action.payload
+        }
+        case "decrease":{
+            return state - action.payload
+        }
+         default:{
+            return state
+        }
+    }
+}
 function App() {
+    const [counter, dispatch] = useReducer(counterReducer, 0)
     return (
     <>
-        <StudentForm/>
-        <div className="row">
-            <StudentsList/>
-            <StudentsStatus studentStatus={true} title="Present Students"/>
-            <StudentsStatus studentStatus={false} title="Absent Students"/> 
-        </div>
+        <h3>The Value of the counter is {counter}</h3>
+        <button className='btn btn-primary' onClick={()=>dispatch({type:"increase", payload:1})}>Increase By 1</button>
+        <button className='btn btn-danger' onClick={()=>dispatch({type:"decrease", payload:1})}>Decrease By 1</button>
+        <button className='btn btn-primary' onClick={()=>dispatch({type:"increase", payload:5})}>Increase By 5</button>
+        <button className='btn btn-danger' onClick={()=>dispatch({type:"decrease", payload:3})}>Increase By 3</button>
     </>
     )
 }
